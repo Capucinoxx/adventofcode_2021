d38e8645db1b89728c514d4ee9fbb63ae43fa0f7
Capucinoxx <bendertermebc@gmail.com>
2021-12-02 02:08:14 -0500

    [feat] - solution défi journée 2 `advent of code` édition 2021

---
+++ day_2/solution.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+# Code réalisé lords de calendrier de l'avent `advent of code` édition 2021. https://adventofcode.com/
+#
+# La réalisation à ce défi à été réalisé par Benjamin Courchesne le 02 décembre 2021.
+
+input = File.read("./input.txt")
+
+# --- Part One ---
+def sol_day2_part_one(input = "")
+  pos = { "forward" => 0, "up" => 0, "down" => 0 }
+  input.split(/ |\n/).each_slice(2) { |k, v| pos[k] += v.to_i }
+  pos["forward"] * (pos["down"] - pos["up"])
+end
+
+print "solution to challenge day 2, part 1: ", sol_day2_part_one(input), "\n"
+
+# --- Part Two ---
+def sol_day2_part_two(input = "")
+  pos = { "forward" => 0, "up" => 0, "down" => 0 }
+  aim = 0
+  input.split(/ |\n/).each_slice(2) do |k, v|
+    pos[k] += v.to_i; aim += if k[0] == "f" then v.to_i * (pos["down"] - pos["up"]) else 0 end
+  end
+  aim * pos["forward"]
+end
+
+print "solution to challenge day 2, part 2: ", sol_day2_part_two(input), "\n"
